f7e42e0981bd1590d1d763084d1a137b370d2f2d
Sebastian Romero <sebastian.romero@imt-atlantique.net>
2022-10-01 16:36:21 +0200

    tutorial gRPC

--- movie/movie.py
+++ movie/movie.py
@@ -10,6 +10,17 @@ class MovieServicer(movie_pb2_grpc.MovieServicer):
         with open('{}/data/movies.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["movies"]
 
+    def GetMovieByID(self, request, context):
+        for movie in self.db:
+            if movie['id'] == request.id:
+                print("Movie found!")
+                return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'], id=movie['id'])
+        return movie_pb2.MovieData(title="", rating=0.0, director="", id="")
+
+    def GetListMovies(self, request, context):
+        for movie in self.db:
+            yield movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'], id=movie['id'])
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     movie_pb2_grpc.add_MovieServicer_to_server(MovieServicer(), server)
